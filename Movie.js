29c61c52103f3b9d6368b47a8c413a2d0e9d83d0
Shalise Franklin <sgf16b@my.fsu.edu>
2018-11-30 20:54:45 +0000

    Lesson 8 final commit

--- Movie.js
+++ Movie.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
 
             $.ajax({
                 //Insert YOUR API Key after the '=' inside the single quotes.
-                url:'https://api.themoviedb.org/3/search/movie?api_key=INSERT-HERE',
+                url:'https://api.themoviedb.org/3/search/movie?api_key=2319db3eb5e2a19bbe0f9782724eeb76&query=big',
                 data: query
             })
             
@@ -58,7 +58,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/                
                 
-                
+                $("#overview").html(json.results[0].overview);
                 
 
                 /***********************Retrieve Movie Release Date from Server & Add to HTML Document***********
@@ -73,7 +73,8 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/               
 
-
+                $("#release_date").html(json.results[0].release_date);
+                
 
 
                  /***********************Retrieve Movie Popularity Rate from Server & Add to HTML Document******
@@ -88,7 +89,8 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/   
 
-  
+                $("#popularity").html(json.results[0].popularity);
+                
 
                 
                  /*****************Retrieve Movie Original Language from Server & Add to HTML Document***********
@@ -103,7 +105,8 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/                 
 
-
+                $("#original_language").html(json.results[0].original_language);
+                
 
                 //The following code retrieves the movie poster path from the JSON object retrieved from the TMDB 
                 //Server and places the poster image on the HTML page, where the id="poster_path".  It took TWO
--- README.md
+++ README.md
@@ -4,21 +4,18 @@
 LIS 2360:  Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+December, 2018.
 
 ### Student Name:  
-(First and Last Name)
+Shalise Franklin.
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+In this assignment, we are designing an application that will display different information about different movies. We will be using jQuery and AJAX methods to parse JSON data returned from a 3rd party server, TheMovieDB (TMDB) API, to access the movie data. The user will enter the name of a movie, and the program will display the movie title, movie plot, release date, popularity rating, language, and the poster. 
 
 ### View Project:
-(Replace this statement with your Github Page URL that was created when you 
- published the project.)
+https://sgf16b.github.io/lesson8_ajax/.
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
-4. (Briefly describe a lesson/concept learned in this lesson.)
-5. (Briefly describe a lesson/concept learned in this lesson.)
+1. Application-programming interface (API)- AN Application-programming interface, or API, is a set of programming instructions and standards for accessing a Web-based software application or Web tool.
+2. Asynchronous JavaScript and XML (AJAX)- Asynchronous JavaScript and XML, or AJAX, is a technique that allows for creating better, faster, and more interactive web applications with the help of XML/JSON, HTML, CSS, and JavaScript. 
+3. JavaScript Object Notation (JSON)- JavaScript Object Notation, JSON, is a lightweight data interchange format and a subset of JavaScript. It is considered easy to use and understand and is language independent.
